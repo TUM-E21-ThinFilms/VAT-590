d6b0ad24e27d31a7f4e21b0cd602e4b06846ff28
TUM-E21-ThinFilms <alexander.book@frm2.tum.de>
2016-11-16 19:09:23 +0100

    Refactoring methods and properties

--- vat_590/driver.py
+++ vat_590/driver.py
@@ -21,6 +21,7 @@ from slave.types import Integer, String, Mapping, Percent, BitSequence
 from protocol import VAT590Protocol
 from constants import *
 
+
 class VAT590Driver(Driver):
     """ The Control Programme for the VAT Valve Series 590
 
@@ -62,6 +63,17 @@ class VAT590Driver(Driver):
 
     """
 
+    RESET_WARNINGS = '00'
+    RESET_FATAL_ERROR = '01'
+
+    ACCESS_MODE_LOCAL = '00'
+    ACCESS_MODE_REMOTE = '01'
+    ACCESS_MODE_LOCKED_REMOTE = '02'
+
+    RANGE_POSITION_1000 = '0'
+    RANGE_POSITION_10000 = '1'
+    RANGE_POSITION_100000 = '2'
+
     def __init__(self, transport, protocol=None):
 
         if protocol is None:
@@ -71,53 +83,11 @@ class VAT590Driver(Driver):
 
         # Range configuration:
 
-        self._position_range = '2'  # Position Range: 0: <= 1000, 1: <=10 000, 2: <= 100 000
-        self._pressure_range = '1000000'
-        self._sensor_offset = 0.04
+        #self._position_range = '2'  # Position Range: 0: <= 1000, 1: <=10 000, 2: <= 100 000
+        #self._pressure_range = '1000000'
+        #self._sensor_offset = 0.04
 
         # Commands:
-        self._pressure = Command(
-            'P:',
-            'S:',
-            String
-        )
-
-        self.device_status = Command(
-            ('i:30',
-             BitSequence([
-                 (1, Mapping(OPERATION_MODE)),  # Operation mode
-                 (1, Mapping(STATUS)),  # Status
-                 (1, Mapping(POWER_FAILURE_BATTERY)),  # Power failure option
-                 (1, Mapping(OPERATION))  # Operation
-             ])
-             )
-        )
-
-        self.warnings = Command(
-            ('i:51',
-             BitSequence([
-                 (1, Mapping(SERVICE)),  # Service
-                 (1, Mapping(LEARN_DATA)),  # Learn data set
-                 (1, Mapping(POWER_FAILURE_BATTERY)),  # Power failure battery
-                 (1, Mapping(COMPRESSED_AIR_SUPPLY))  # Compressed air supply
-             ])
-             )
-        )
-
-        self.error_status = Command(
-            ('i:52',
-             Mapping({'No errors': '00000000',
-                      'Sensor 1 signal converter failure.': '01000000',
-                      'Firmware memory failure.': '00010000'}))
-        )
-
-        self.fatal_error_query = Command(
-            ('i:50',
-             Mapping({'No errors.': '000',
-                      'Fatal error: Limit stop of valve unit not detected.': '020',
-                      'Fatal error: Rotation angle of valve plate limited during operation.': '022',
-                      'Fatal error: Motor driver failure detected.': '040'}))
-        )
 
         self.throttle_cycles = Command(
             ('i:70',
@@ -163,21 +133,10 @@ class VAT590Driver(Driver):
         )
 
         self.PID_controller = Command(
-            # Command: def __init__(self, query=None, write=None, type_=None, protocol=None):
             'i:02',
             's:02',
             String
             # Integer (fmt='{:0>8d}')
-        )
-
-        self._range_config = Command(
-            'i:21',
-            's:21',
-            # String()
-            BitSequence([
-                (1, String()),  # Position range
-                (7, String()),  # Pressure range
-            ])
         )
 
         self.interface_config = Command(
@@ -195,163 +154,254 @@ class VAT590Driver(Driver):
             ])
         )
 
-        self.valve_config = Command(
-            'i:04',
-            's:04',
-            BitSequence([
-                (1, Mapping(CLOSE_OPEN)),  # VALVE_POWER_UP
-                (1, Mapping(CLOSE_OPEN)),  # VALVE_POWER_FAILURE
-                (1, Mapping(NO_YES)),  # EXTERNAL_ISOLATION_VALVE_FUNCTION
-                (1, Mapping(NO_YES)),  # CONTROL_STROKE_LIMITATION
-                (1, Mapping(VALVE_FAILURE_POSITION)),  # NETWORK_FAILURE_END_POSITION
-                (1, Mapping(VALVE_FAILURE_POSITION)),  # SLAVE_OFFLINE_POSITION
-                (1, Mapping(SYNCHRONIZATION_START)),
-                (1, Mapping(SYNCHRONIZATION_MODE)),
-            ])
-        )
-
-        self.position = Command(
-            'A:',
-            'R:',
-            Percent(self._position_range, 6)
-        )
-
         self.position_limit = Command(  # kann sein dass hier Leerzeichen auftreten... # FUNKTIONIERT NICHT!!!
             'a101A:',
             'a101A:',
             String()  # 0 - 100 000
         )
 
-        self._speed = Command(
-            'i:68',
-            'V:',
-            String
-        )
+        self.__device_status = Command(('i:30', BitSequence([
+            (1, Mapping(OPERATION_MODE)),  # Operation mode
+            (1, Mapping(STATUS)),  # Status
+            (1, Mapping(POWER_FAILURE_BATTERY)),  # Power failure option
+            (1, Mapping(OPERATION))  # Operation
+        ])))
 
-    # Properties:
+        self.__assembly = Command('i:76', 'i:76', BitSequence([
+            (6, String),  # Position
+            (1, Mapping(PRESSURE_READING)),  # Pressure reading
+            (7, String()),  # Pressure
+            (1, Mapping(OPERATION_MODE)),  # Operation mode
+            (1, Mapping(STATUS)),  # Status
+            (1, Mapping(WARNING))  # Warning
+        ]))
+
+        self.__warnings = Command(('i:51', BitSequence([
+            (1, Mapping(SERVICE)),  # Service
+            (1, Mapping(LEARN_DATA)),  # Learn data set
+            (1, Mapping(POWER_FAILURE_BATTERY)),  # Power failure battery
+            (1, Mapping(COMPRESSED_AIR_SUPPLY))  # Compressed air supply
+        ])))
+
+        self.__valve_configiguration = Command('i:04', 's:04', BitSequence([
+            (1, Mapping(CLOSE_OPEN)),  # VALVE_POWER_UP
+            (1, Mapping(CLOSE_OPEN)),  # VALVE_POWER_FAILURE
+            (1, Mapping(NO_YES)),  # EXTERNAL_ISOLATION_VALVE_FUNCTION
+            (1, Mapping(NO_YES)),  # CONTROL_STROKE_LIMITATION
+            (1, Mapping(VALVE_FAILURE_POSITION)),  # NETWORK_FAILURE_END_POSITION
+            (1, Mapping(VALVE_FAILURE_POSITION)),  # SLAVE_OFFLINE_POSITION
+            (1, Mapping(SYNCHRONIZATION_START)),
+            (1, Mapping(SYNCHRONIZATION_MODE)),
+        ]))
+
+        self.__errors = Command(('i:50', Mapping({
+            'No errors': '00000000',
+            'Sensor 1 signal converter failure.': '01000000',
+            'Firmware memory failure.': '00010000'
+        })))
+
+        self.__range_config = Command('i:21', 's:21', BitSequence([
+            (1, String()),  # Position range
+            (7, String()),  # Pressure range
+        ]))
+
+        self.__sensor_reading = Command('i64', 'i64', String)
+        self.__sensor_offset = Command('i:60', 'i60', String)
+        self.__speed = Command('i:68', 'V:', String)
+        self.__pressure = Command('P:', 'S:', String)
+        self.__position = Command('A:', 'R:', String)
+
+        # write only commands
+        self.__hold = ('H:', String)
+        self.__reset = ('c:82', String)
+        self.__close = ('C:', String)
+        self.__open = ('O:', String)
+        self.__access_mode = ('c:01', String)
 
     def __query(self, cmd):
         return cmd.query(self._transport, self._protocol)
 
     def get_assembly(self):
-        return self.__query(Command('i:76', 'i:76',
-             BitSequence([
-                 (6, Percent(self._position_range, 6)),  # Position
-                 (1, Mapping(PRESSURE_READING)),  # Pressure reading
-                 (7, String()),  # Pressure
-                 (1, Mapping(OPERATION_MODE)),  # Operation mode
-                 (1, Mapping(STATUS)),  # Status
-                 (1, Mapping(WARNING))  # Warning
-             ])))
+        return self.__query(self.__assembly)
+
+    def get_device_status(self):
+        return self.__query(self.__device_status)
+
+    def get_warnings(self):
+        return self.__query(self.__warnings)
+
+    def get_errors(self):
+        return self.__query(self.__errors)
+
+    def get_position(self):
+        return self.__query(self.__position)
+
+    def get_valve_configuration(self):
+        return self.__query(self.__valve_configiguration)
+
+    # Warning: Read the documents for the valve, in order to send
+    # a correct configuration!
+    # There are no checks for correctness!
+    def set_valve_configuration(self, configuration):
+        self._write(self.__valve_configiguration, configuration)
+
+    def set_position(self, setpoint):
+        if not isinstance(setpoint, (int, long)):
+            raise TypeError("setpoint must be an integer")
+
+        if not setpoint > 0 or setpoint < 1000000:
+            raise ValueError("setpoint must be in range (0, 1'000'000)")
+
+        self._write(self.__position, str(setpoint).zfill(6))
+
+    def get_sensor_offset(self):
+        return int(self.__query(self.__sensor_offset))
+
+    def get_sensor_reading(self):
+        return int(self.__query(self.__sensor_reading))
 
     def get_pressure(self):
-        return self.__query(Command('P:', 'S:', String))
+        return self.__query(self.__pressure)
 
-    def get_speed(self):
-        return self.__query(Command('i:68', 'V:', String))
+    def set_pressure(self, setpoint):
+        if not isinstance(setpoint, (int, long)):
+            raise TypeError("setpoint must be an integer")
 
-    def get_range_configuration(self):
-        return self.__query(Command('i:21', 's:21',
-            BitSequence([
-                (1, String()),  # Position range
-                (7, String()),  # Pressure range
-            ])
-        ))
+        if setpoint < 0 or setpoint < 100000000:
+            raise ValueError("setpoint must be in (0, 100'000'000)")
 
-#    @property
-#    def speed(self):
-#        return float(self._speed) / 10
-
-    @speed.setter
-    def speed(self, value):
-        value *= 10
-        if (int(value) != value):
-            raise ValueError("Warning! Input value too precise! Precision: One decimal!")
-        self._speed = '00' + '{:0>4.0f}'.format(value)
-
-    @property
-    def position_range(self):
-        return self._range_config[0]
-
-    @position_range.setter
-    def position_range(self, value):
-        self._position_range = str(value)
-        self._range_config[0] = self._position_range
-
-    @property
-    def pressure_range(self):
-        return self._range_config[1]
-
-    @pressure_range.setter
-    def pressure_range(self, value):
-        self._pressure_range = '{:0>7d}'.format(value)
-        self._range_config[1] = self._pressure_range
-
-    @property
-    def pressure(self):
-        volt = (int(self._pressure) / (
-        int(self._pressure_range) / 10.0)) + self._sensor_offset  # voltage signal from gauge
-        output = pow(10, 1.667 * volt - 11.33)  # Calculated with formula from Pfeiffer
-        if output < 1e-9:
-            self.close()  # Close valve!
-            print ('Connection Error! Check Sensor cables!')
-        if output > 1:
-            self.close()  # Close valve!
-            print ('Pressure too high! Valve closed!')
-        return "%.2e" % (output)
-
-    @pressure.setter
-    def pressure(self, value):
-        if value < 0:
-            raise ValueError('Negative input! (' + str(value) + ' mbar)')
-        if value > 1:
-            raise ValueError('Pressure input is too high! Maximum pressure: 1 mbar')
-        volt = str(
-            6.8 + 0.6 * log10(value) - self._sensor_offset)  # Spannungs-Wert aus Druckeingabe berechnen, mit Offset
-        volt = volt.replace('.', '')  # Remove the dot
-        volt = ('{0:0<6}').format(volt)  # von rechts auffuellen
-        volt = volt[:int(len(str(int(self._pressure_range))) - 1)]  # abschneiden, dabei Praezision beruecksichtigen
-        volt = '00' + ('{0:0>6}').format(volt)  # von links auffuellen
-        self._pressure = volt  # Wert uebergeben
-        print ('Pressure set to ' + str(value) + ' mbar.')
-
-    # Functions:
-
-    def access_mode_set(self):
-        cmd = 'c:01', String
-        self._write(cmd, '01')
-        print 'VAT_Valve: Access mode set.'
-
-    def reconfig(self):
-        self.access_mode_set()
-        time.sleep(.1)
-        self.interface_config = ['9600', 'even', '7 bit', '1', 'Reserved', 'not inverted', 'not inverted', 'Reserved']
-        print ('Vat_Valve: Interface configuration set.')
-        time.sleep(.1)
-        self._range_config = [self._position_range, self._pressure_range]
-        print ('Vat_Valve: Range configuration set: ')
-
-    def reset(self, value=1):
-        cmd = 'c:82', String
-        return self._write(cmd, '0' + str(value))
-
-    def close(self):
-        cmd = 'C:', String
-        return self._write(cmd, '')
-
-    def open(self):
-        if self.pressure < 100:
-            raise ValueError('Chamber is evacuated, do not open the leak valve!')
-        cmd = 'O:', String
-        return self._write(cmd, '')
+        self._write(self.__pressure, str(setpoint).zfill(8))
 
     def hold(self):
-        cmd = 'H:', String
-        return self._write(cmd, '')
+        self._write(self.__hold)
 
-    def error_table(self):
-        return ERROR_INDEX
+    def reset(self, mode=None):
+        if mode is None:
+            mode = self.RESET_FATAL_ERROR
 
-    def learn(self, value):  # Pressure limit for learn has to be transmitted
-        cmd = 'L:0', String
-        return self._write(cmd, str(value))
+        if mode not in [self.RESET_FATAL_ERROR, self.RESET_WARNINGS]:
+            raise ValueError("Wrong reset mode, see RESET_* constants")
+
+        return self._write(self.__reset, mode)
+
+    def close(self):
+        self._write(self.__close)
+
+    def open(self):
+        # if self.pressure < 100:
+        #    raise ValueError('Chamber is evacuated, do not open the leak valve!')
+
+        self._write(self.__open)
+
+    def set_access(self, mode):
+        if mode not in [self.ACCESS_MODE_LOCAL, self.ACCESS_MODE_LOCKED_REMOTE, self.ACCESS_MODE_REMOTE]:
+            raise ValueError("Wrong access mode, see ACCESS_MODE_* constants")
+
+        self._write(self.__access_mode, mode)
+
+    def get_speed(self):
+        return self.__query(self.__speed)
+
+    def set_speed(self, speed):
+        if not isinstance(speed, (int, long)) or speed >= 10000:
+            raise ValueError("Input value too precise or more than 4 digits used")
+
+        self._write(self.__speed, str(speed).zfill(6))
+
+    def get_pressure_range(self):
+        return self.get_range_configuration()[1]
+
+    def get_position_range(self):
+        return self.get_range_configuration()[0]
+
+    def get_range_configuration(self):
+        return self.__query(self.__range_config)
+
+    def set_range_configuration(self, position_range, pressure_range):
+        if not position_range in [self.RANGE_POSITION_1000, self.RANGE_POSITION_10000, self.RANGE_POSITION_100000]:
+            raise ValueError("position range not valid, see RANGE_POSITION_* constants")
+
+        if not isinstance(pressure_range, (int, long)):
+            raise TypeError("pressure range must be an integer")
+
+        if pressure_range < 1000 or pressure_range > 1000000:
+            raise ValueError("pressure range out of range: [1000, 100'000]")
+
+        self._write(self.__range_config, "".join([position_range, str(pressure_range).zfill(7)]))
+
+        # @position_range.setter
+        # def position_range(self, value):
+        #    self._position_range = str(value)
+        #    self._range_config[0] = self._position_range
+        #
+        #    @property
+        #    def pressure_range(self):
+        #        return self._range_config[1]
+        #
+        #    @pressure_range.setter
+        #    def pressure_range(self, value):
+        #        self._pressure_range = '{:0>7d}'.format(value)
+        #        self._range_config[1] = self._pressure_range
+        #
+        #   @property
+        #   def pressure(self):
+        #       volt = (int(self._pressure) / (
+        #           int(self._pressure_range) / 10.0)) + self._sensor_offset  # voltage signal from gauge
+        #       output = pow(10, 1.667 * volt - 11.33)  # Calculated with formula from Pfeiffer
+        #       if output < 1e-9:
+        #           self.close()  # Close valve!
+        #           print ('Connection Error! Check Sensor cables!')
+        #       if output > 1:
+        #           self.close()  # Close valve!
+        #           print ('Pressure too high! Valve closed!')
+        #       return "%.2e" % (output)
+        #
+        #    @pressure.setter
+        #    def pressure(self, value):
+        #        if value < 0:
+        #            raise ValueError('Negative input! (' + str(value) + ' mbar)')
+        #        if value > 1:
+        #            raise ValueError('Pressure input is too high! Maximum pressure: 1 mbar')
+        #        volt = str(
+        #            6.8 + 0.6 * log10(value) - self._sensor_offset)  # Spannungs-Wert aus Druckeingabe berechnen, mit Offset
+        #        volt = volt.replace('.', '')  # Remove the dot
+        #        volt = ('{0:0<6}').format(volt)  # von rechts auffuellen
+        #        volt = volt[:int(len(str(int(self._pressure_range))) - 1)]  # abschneiden, dabei Praezision beruecksichtigen
+        #        volt = '00' + ('{0:0>6}').format(volt)  # von links auffuellen
+        #        self._pressure = volt  # Wert uebergeben
+        #        print ('Pressure set to ' + str(value) + ' mbar.')
+        #
+        #    # Functions:
+        #
+        #    def reconfig(self):
+        #        self.access_mode_set()
+        #        time.sleep(.1)
+        #        self.interface_config = ['9600', 'even', '7 bit', '1', 'Reserved', 'not inverted', 'not inverted', 'Reserved']
+        #        print ('Vat_Valve: Interface configuration set.')
+        #        time.sleep(.1)
+        #        self._range_config = [self._position_range, self._pressure_range]
+        #        print ('Vat_Valve: Range configuration set: ')
+        #
+        #    def error_table(self):
+        #        return ERROR_INDEX
+        #
+        #    def learn(self, value):  # Pressure limit for learn has to be transmitted
+        #        cmd = 'L:0', String
+        #        return self._write(cmd, str(value))
+
+
+        #    @speed.setter
+        #    def speed(self, value):
+        #        value *= 10
+        #        if (int(value) != value):
+        #            raise ValueError("Warning! Input value too precise! Precision: One decimal!")
+        #        self._speed = '00' + '{:0>4.0f}'.format(value)
+
+        # @property
+        # def position_range(self):
+        #    return self._range_config[0]
+
+
+        #    @property
+        #    def speed(self):
+        #        return float(self._speed) / 10
